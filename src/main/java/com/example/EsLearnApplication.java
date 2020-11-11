2f5b0b5b11f2396d043a088427b1024d80a539ae
安超 <240853850@qq.com>
2020-11-11 17:38:12 +0800

    Initial commit

---
+++ src/main/java/com/example/EsLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/EsLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsLearnApplicationTests {
+
+    @Test
+    public void testInit() {
+        TransportClient
+
+    }
+
+}
