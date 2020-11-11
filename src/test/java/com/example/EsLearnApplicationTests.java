f67b15ed8d8e867599354bcaabc1d6ed79293b88
安超 <240853850@qq.com>
2020-11-11 17:57:36 +0800

    init

--- src/test/java/com/example/EsLearnApplicationTests.java
+++ src/test/java/com/example/EsLearnApplicationTests.java
@@ -1,16 +1,54 @@
 package com.example;
 
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.transport.TransportClient;
-import org.junit.jupiter.api.Test;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootTest
-class EsLearnApplicationTests {
+public class EsLearnApplicationTests {
 
-    @Test
-    public void testInit() {
-        TransportClient
+    private TransportClient transportClient;
 
+
+    @Before
+    public void startTransportClient() throws UnknownHostException {
+        this.transportClient = new PreBuiltTransportClient(Settings.EMPTY);
+        transportClient.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.1.184"), 9300));
     }
 
+    @After
+    public void shutdownTransportClient(){
+        transportClient.close();
+    }
+
+    @Test
+    public void testCreateIndex(){
+        CreateIndexResponse indexResponse = transportClient.admin().indices().prepareCreate("dangdang").get();
+        if (indexResponse.isAcknowledged()) {
+            System.out.println("索引创建成功");
+        }else {
+            System.err.println("索引创建失败");
+        }
+    }
+
+    @Test
+    public void testDeleteIndex(){
+        AcknowledgedResponse acknowledgedResponse = transportClient.admin().indices().prepareDelete("dangdang").get();
+        if (acknowledgedResponse.isAcknowledged()) {
+            System.out.println("索引删除成功");
+        }else{
+            System.out.println("索引删除失败");
+        }
+    }
 }
